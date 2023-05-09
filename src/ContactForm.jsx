88730058579a28399082b8f0252aedf7cfc0de78
Wajahat Kanju <wajahat5ahmad@gmail.com>
2023-05-09 16:08:54 +0500

    fix form design(i.e) give user feedback when needed

--- src/ContactForm.jsx
+++ src/ContactForm.jsx
@@ -1,13 +1,42 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./styles/ContactForm.scss";
 
 function ContactForm() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
+  const [submissionStatus, setSubmissionStatus] = useState(null);
+
+  const [statusMessage, setStatusMessage] = useState("");
+
+  useEffect(() => {
+    switch (submissionStatus) {
+      case "success":
+        setStatusMessage(
+          <p className="success-message">Thanks for contacting us!</p>
+        );
+        break;
+      case "submission_in_progress":
+        setStatusMessage(
+          <p className="progress-message">Submitting The Form Please wait</p>
+        );
+        break;
+      case "error":
+        setStatusMessage(
+          <p className="error-message">
+            There was an error submitting the form. Please try again later.
+          </p>
+        );
+        break;
+      default:
+        setStatusMessage("");
+        break;
+    }
+  }, [submissionStatus]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setSubmissionStatus("submission_in_progress");
     const data = {
       name: name,
       email: email,
@@ -28,51 +57,75 @@ function ContactForm() {
         setName("");
         setEmail("");
         setMessage("");
-        console.log(response);
+        setSubmissionStatus("success");
       } else {
         throw new Error("Network response was not ok");
       }
     } catch (error) {
       console.log(error);
+      setSubmissionStatus("error");
     }
   };
 
+  const handleBlur = (event) => {
+    event.target.classList.add("blur");
+    if (event.target.value === "") {
+      event.target.classList.add("empty");
+    } else {
+      event.target.classList.remove("empty");
+    }
+  }
+
   return (
-    <section className="contact__form" id="contact">
-      <div className="contact__form__container">
-        <h2 className="form__header">Contact Me</h2>
+    <section className="contact-form" id="contact">
+      <div className="contact-form__container">
+        <h2 className="contact-form__header">Contact Me</h2>
+        <div className="status-message">{statusMessage}</div>
+
         <form
           method="POST"
           acceptCharset="UTF-8"
           id="form"
           onSubmit={handleSubmit}
         >
-          <label htmlFor="name">Name:</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            required
-          />
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-          <label htmlFor="message">Message:</label>
-          <textarea
-            id="message"
-            name="message"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            required
-          />
+          <div className="form-input-container">
+            <input
+            className="empty"
+              type="text"
+              id="name"
+              name="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              onBlur={handleBlur}
+              required
+            />
+            <label htmlFor="name">Name</label>
+          </div>
+          <div className="form-input-container">
+            <input
+            className="empty"
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              onBlur={handleBlur}
+              required
+            />
+            <label htmlFor="email">Email</label>
+          </div>
+          <div className="form-input-container">
+            <textarea
+            className="empty"
+              id="message"
+              name="message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              onBlur={handleBlur}
+              required
+            />
+            <label htmlFor="message">Message</label>
+          </div>
           <input type="hidden" name="_gotcha" style={{ display: "none" }} />
           <button type="submit">Submit</button>
         </form>
