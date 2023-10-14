b0c0e14f8faff0227d376c3abf0a350436f7ae2b
WajahatKanju <wajahat5ahmad@gmail.com>
2023-10-14 16:12:50 +0500

    update projects

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,47 +1,71 @@
-import './styles/Projects.scss';
+import "./styles/Projects.scss";
+
+import todoImage from "./assets/projects/todo.png";
+import commercePng from "./assets/projects/commerce.png";
+import lemonPng from "./assets/projects/lemon.png";
+import calculatorPng from "./assets/projects/calculator.png";
 
 const Projects = () => {
   const projects = [
     {
       id: 1,
-      title: 'Todo List',
-      description: 'A simple todo list application that allows users to add and complete tasks.',
-      image: 'https://via.placeholder.com/300x200',
-      link: 'https://www.example.com/todolist'
+      title: "Todo List",
+      description:
+        "A simple todo list application that allows users to add and complete tasks.",
+      image: todoImage,
+      link: "https://github.com/WajahatKanju/react-todo-list",
     },
+
     {
       id: 2,
-      title: 'Weather App',
-      description: 'A weather application that displays current weather conditions for a given location.',
-      image: 'https://via.placeholder.com/300x200',
-      link: 'https://www.example.com/weatherapp'
+      title: "E-commerce Store",
+      description:
+        "An online store that allows users to browse and purchase products.",
+      image: commercePng,
+      link: "https://github.com/WajahatKanju/dj-commerce",
     },
     {
       id: 3,
-      title: 'E-commerce Store',
-      description: 'An online store that allows users to browse and purchase products.',
-      image: 'https://via.placeholder.com/300x200',
-      link: 'https://www.example.com/ecommerce'
+      title: "Little Lemon App",
+      description:
+        "For my Meta Frontend Developer Program capstone project, I endeavored to build an advanced e-commerce platform that prioritizes user experience and engagement. ",
+      image: lemonPng,
+      link: "https://wajahatkanju.github.io/little-lemon-front/",
     },
     {
       id: 4,
-      title: 'Recipe App',
-      description: 'A recipe application that displays a list of recipes and allows users to search and filter them.',
-      image: 'https://via.placeholder.com/300x200',
-      link: 'https://www.example.com/recipeapp'
-    }
+      title: "React Calculator for Fun",
+      description: "I created this React calculator as a hobby project.",
+      image: calculatorPng,
+      link: "https://wajahatkanju.github.io/basic_react_calculator/",
+    },
   ];
 
   return (
-    <section className="projects" id="projects" aria-labelledby="projects-heading">
+    <section
+      className="projects"
+      id="projects"
+      aria-labelledby="projects-heading"
+    >
       <h2 id="projects-heading">My Projects</h2>
       <div className="project-list" role="list">
-        {projects.map(project => (
+        {projects.map((project) => (
           <div className="project" role="listitem" key={project.id}>
-            <img src={project.image} alt={`Screenshot of ${project.title}`} />
+            <img
+              src={project.image}
+              alt={`Screenshot of ${project.title}`}
+              className="project-image"
+            />
             <h3>{project.title}</h3>
             <p>{project.description}</p>
-            <a href={project.link} aria-label={`Visit ${project.title} project`}>View Project</a>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href={project.link}
+              aria-label={`Visit ${project.title} project`}
+            >
+              View Project
+            </a>
           </div>
         ))}
       </div>
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  base: "/portfolio/",
+  base: "/",
   plugins: [react()],
   build: {
     outDir: 'dist',
