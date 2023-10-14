b8883ded7da52d4c6157c07b0835486c98460d76
WajahatKanju <wajahat5ahmad@gmail.com>
2023-10-14 22:52:16 +0500

    updated scripts

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  base: "/",
+  base: "",
   plugins: [react()],
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    sourcemap: true,
     rollupOptions: {
       output: {
         entryFileNames: '[name]-[hash].js',
